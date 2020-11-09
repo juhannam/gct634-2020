a3f890cba0819e8ede2899e916970043450ffb12
Taegyun Kwon <ilcobo2@kaist.ac.kr>
2020-11-09 13:01:54 +0000

    add save in evaluate.py, update leaderboard link

--- hw3/evaluate.py
+++ hw3/evaluate.py
@@ -1,19 +1,21 @@
 from collections import defaultdict
+from pathlib import Path
 
 import numpy as np
 import torch as th
 import torch.nn as nn
 from torch.utils.data import DataLoader
-from mir_eval.util import midi_to_hz
+from mir_eval.util import midi_to_hz, hz_to_midi
 from mir_eval.transcription import precision_recall_f1_overlap as evaluate_notes
+import pretty_midi
+from mido import MidiFile, MidiTrack, Message
+import soundfile
 
 from dataset import MAESTRO_small, allocate_batch
 from constants import HOP_SIZE, SAMPLE_RATE, MIN_MIDI
 
-from mir_eval.util import midi_to_hz
 
-
-def evaluate(model, batch, device):
+def evaluate(model, batch, device, save=False, save_path=None):
     metrics = defaultdict(list)
     batch = allocate_batch(batch, device)
 
@@ -62,6 +64,17 @@ def evaluate(model, batch, device):
         metrics['metric/note-with-offsets/recall'].append(r)
         metrics['metric/note-with-offsets/f1'].append(f)
         metrics['metric/note-with-offsets/overlap'].append(o)
+
+        if save:
+            if len(p_est) == 0:
+                print(f'no onset detected. skip: {batch["path"][n].stem}')
+            midi_filename = Path(save_path).parent / (Path(batch['path'][n]).stem + '.midi')
+            save_midi(midi_filename, p_est, i_est, [64] * len(p_est))
+
+            wav_filename = Path(save_path).parent / (Path(batch['path'][n]).stem + '.wav')
+            midi_file = pretty_midi.PrettyMIDI(str(midi_filename))
+            synth_audio = midi_file.fluidsynth(fs=16000)
+            soundfile.write(wav_filename, synth_audio, 16000)
 
     return metrics
 
@@ -127,3 +140,37 @@ def framewise_eval(pred, label, threshold=0.5):
     f1 = 2 * pr * re / float(pr + re) if (pr + re) > 0 else 0
     
     return pr, re, f1
+
+
+def save_midi(path, pitches, intervals, velocities):
+    """
+    Save extracted notes as a MIDI file
+    Parameters
+    ----------
+    path: the path to save the MIDI file
+    pitches: np.ndarray of bin_indices
+    intervals: list of (onset_index, offset_index)
+    velocities: list of velocity values
+    """
+    file = MidiFile()
+    track = MidiTrack()
+    file.tracks.append(track)
+    ticks_per_second = file.ticks_per_beat * 2.0
+
+    events = []
+    for i in range(len(pitches)):
+        events.append(dict(type='on', pitch=pitches[i], time=intervals[i][0], velocity=velocities[i]))
+        events.append(dict(type='off', pitch=pitches[i], time=intervals[i][1], velocity=velocities[i]))
+    events.sort(key=lambda row: row['time'])
+
+    last_tick = 0
+    for event in events:
+        current_tick = int(event['time'] * ticks_per_second)
+        velocity = int(event['velocity'] * 127)
+        if velocity > 127:
+            velocity = 127
+        pitch = int(round(hz_to_midi(event['pitch'])))
+        track.append(Message('note_' + event['type'], note=pitch, velocity=velocity, time=current_tick - last_tick))
+        last_tick = current_tick
+
+    file.save(path)
